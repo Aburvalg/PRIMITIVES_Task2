9d9700e35cae34414ac3a48aa25f7a46d182db22
Andrey <3451z@mail.ru>
2021-12-25 03:40:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double balance = 100.76;
+        double payment = 1223.31;
+
+        if(payment >= 1000) {
+            int bonus = (int) (payment / 100); // 1 руб. за каждые 100 руб.
+            double total = bonus + payment + balance;
+
+            System.out.println("Ваш баланс " + total + " рублей");
+            System.out.println("Вам начислено " + bonus + " баллов");
+
+        }        else {
+            double total = balance + payment;
+
+            System.out.println("Ваш баланс " + total + " рублей");
+            System.out.println("Вам начислено 0 баллов");
+        }
+    }
+}
