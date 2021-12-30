baaca4634a994b56533aad86459fb07b942f56ef
Andrey <3451z@mail.ru>
2021-12-30 06:25:57 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         double balance = 100.76;
         double payment = 1223.31;
 
-        if(payment >= 1000) {
+        if (payment >= 1000) {
             int bonus = (int) (payment / 100); // 1 руб. за каждые 100 руб.
             double total = bonus + payment + balance;
 
             System.out.println("Ваш баланс " + total + " рублей");
             System.out.println("Вам начислено " + bonus + " баллов");
 
-        }        else {
+        } else {
             double total = balance + payment;
 
             System.out.println("Ваш баланс " + total + " рублей");
